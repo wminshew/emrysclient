8a4784aeaba22d216695ddfc68532d45c628dc1e
wminshew <wminshew@gmail.com>
2018-05-04 17:18:37 -0700

    moved request body from buffer to pipe to save memory for client; re-factored creation of multipart form

--- cmd/run.go
+++ cmd/run.go
@@ -2,7 +2,6 @@ package cmd
 
 import (
 	"bufio"
-	"bytes"
 	"fmt"
 	"github.com/mholt/archiver"
 	"github.com/spf13/cobra"
@@ -75,89 +74,83 @@ var runCmd = &cobra.Command{
 }
 
 func postJob(j *job) (*http.Response, error) {
-	bodyBuf := &bytes.Buffer{}
-	bodyWriter := multipart.NewWriter(bodyBuf)
+	r, w := io.Pipe()
+	bodyW := multipart.NewWriter(w)
 
 	log.Printf("Packing request...\n")
-	requirementsWriter, err := bodyWriter.CreateFormFile("requirements", "requirements.txt")
-	if err != nil {
-		log.Printf("Failed to create requirements.txt form file: %v\n", err)
-		return nil, err
-	}
-	requirementsFile, err := os.Open(j.Requirements)
-	if err != nil {
-		log.Printf("Failed to open file %s: %v\n", j.Requirements, err)
-		return nil, err
-	}
-	defer check.Err(requirementsFile.Close)
-	_, err = io.Copy(requirementsWriter, requirementsFile)
-	if err != nil {
-		log.Printf("Failed to copy requirements file: %v\n", err)
-		return nil, err
-	}
+	go func() {
+		defer check.Err(w.Close)
 
-	trainWriter, err := bodyWriter.CreateFormFile("train", "train.py")
-	if err != nil {
-		log.Printf("Failed to create train.py form file: %v\n", err)
-		return nil, err
-	}
-	trainFile, err := os.Open(j.Train)
-	if err != nil {
-		log.Printf("Failed to open file %s: %v\n", j.Train, err)
-		return nil, err
-	}
-	defer check.Err(trainFile.Close)
-	_, err = io.Copy(trainWriter, trainFile)
-	if err != nil {
-		log.Printf("Failed to copy train file %s: %v\n", j.Train, err)
-		return nil, err
-	}
+		err := addFormFile(bodyW, "requirements", "requirements.txt", j.Requirements)
+		if err != nil {
+			log.Printf("Failed to add requirements to POST: %v\n", err)
+			_ = w.CloseWithError(err)
+			return
+		}
 
-	dataTarGzPath := j.Data + ".tar.gz"
-	if err = archiver.TarGz.Make(dataTarGzPath, []string{j.Data}); err != nil {
-		log.Printf("Failed to tar & gzip %s: %v\n", j.Data, err)
-		return nil, err
-	}
-	defer check.Err(func() error { return os.Remove(dataTarGzPath) })
-	dataWriter, err := bodyWriter.CreateFormFile("data", "data.tar.gz")
-	if err != nil {
-		log.Printf("Failed to create data.tar.gz file: %v\n", err)
-		return nil, err
-	}
-	dataTarGzFile, err := os.Open(dataTarGzPath)
-	if err != nil {
-		log.Printf("Failed to open file %s: %v\n", dataTarGzPath, err)
-		return nil, err
-	}
-	defer check.Err(dataTarGzFile.Close)
-	_, err = io.Copy(dataWriter, dataTarGzFile)
-	if err != nil {
-		log.Printf("Failed to copy data.tar.gz: %v\n", err)
-		return nil, err
-	}
+		err = addFormFile(bodyW, "train", "train.py", j.Train)
+		if err != nil {
+			log.Printf("Failed to add train to POST: %v\n", err)
+			_ = w.CloseWithError(err)
+			return
+		}
 
-	err = bodyWriter.Close()
-	if err != nil {
-		log.Printf("Failed to close request bodyWriter: %v\n", err)
-		return nil, err
-	}
+		dataTarGzPath := j.Data + ".tar.gz"
+		if err = archiver.TarGz.Make(dataTarGzPath, []string{j.Data}); err != nil {
+			log.Printf("Failed to tar & gzip %s: %v\n", j.Data, err)
+			_ = w.CloseWithError(err)
+			return
+		}
+		defer check.Err(func() error { return os.Remove(dataTarGzPath) })
+		err = addFormFile(bodyW, "data", "data.tar.gz", dataTarGzPath)
+		if err != nil {
+			log.Printf("Failed to add data to POST: %v\n", err)
+			_ = w.CloseWithError(err)
+			return
+		}
+
+		err = bodyW.Close()
+		if err != nil {
+			log.Printf("Failed to close request bodyW: %v\n", err)
+			_ = w.CloseWithError(err)
+			return
+		}
+	}()
+
 	h := resolveHost()
 	u := url.URL{
 		Scheme: "https",
 		Host:   h,
 		Path:   "/user/job/new",
 	}
-	log.Printf("Sending request...\n")
-	req, err := http.NewRequest("POST", u.String(), bodyBuf)
+	req, err := http.NewRequest("POST", u.String(), r)
 	if err != nil {
 		log.Printf("Failed to create new http request: %v\n", err)
 		return nil, err
 	}
 	req.Header.Set("Authorization", fmt.Sprintf("Bearer %v", j.Token))
-	contentType := bodyWriter.FormDataContentType()
-	req.Header.Set("Content-Type", contentType)
-	req.Header.Set("Content-Encoding", "gzip")
+	req.Header.Set("Content-Type", bodyW.FormDataContentType())
 
 	client := resolveClient()
+	log.Printf("Sending request...\n")
 	return client.Do(req)
+}
+
+func addFormFile(w *multipart.Writer, name, filename, filepath string) error {
+	tempW, err := w.CreateFormFile(name, filename)
+	if err != nil {
+		return err
+	}
+
+	file, err := os.Open(filepath)
+	if err != nil {
+		return err
+	}
+	defer check.Err(file.Close)
+
+	_, err = io.Copy(tempW, file)
+	if err != nil {
+		return err
+	}
+	return nil
 }
