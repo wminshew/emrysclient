a978f5ad7201fe6106a8c5fa8e1a336ed6e45769
wminshew <wminshew@gmail.com>
2018-05-23 21:53:49 -0700

    after winning job auction, request and execute job image

--- cmd/startCmd.go
+++ cmd/startCmd.go
@@ -1,12 +1,13 @@
 package cmd
 
 import (
-	// "compress/zlib"
-	// "context"
-	// "docker.io/go-docker"
-	// "docker.io/go-docker/api/types"
-	// "docker.io/go-docker/api/types/container"
+	"bufio"
 	"bytes"
+	"compress/zlib"
+	"context"
+	"docker.io/go-docker"
+	"docker.io/go-docker/api/types"
+	"docker.io/go-docker/api/types/container"
 	"encoding/json"
 	"fmt"
 	"github.com/gorilla/websocket"
@@ -117,125 +118,115 @@ will default to the mining command provided in
 						}
 						defer check.Err(resp.Body.Close)
 
-						p = path.Join("miner", "job", m.Job.ID.String(), "data")
-						resp, err = get(p, authToken, jobToken)
+						zResp, err := zlib.NewReader(resp.Body)
 						if err != nil {
-							log.Printf("Error GET %v: %v\n", p, err)
+							log.Printf("Error creating zlib img reader: %v\n", err)
 							return
 						}
-						defer check.Err(resp.Body.Close)
+
+						ctx := context.Background()
+						cli, err := docker.NewEnvClient()
+						if err != nil {
+							log.Printf("Error creating docker client: %v\n", err)
+							return
+						}
+						imgLoadResp, err := cli.ImageLoad(ctx, zResp, false)
+						if err != nil {
+							log.Printf("Error loading image: %v\n", err)
+							return
+						}
+						defer check.Err(imgLoadResp.Body.Close)
+						err = zResp.Close()
+						if err != nil {
+							log.Printf("Error closing zlib img reader: %v\n", err)
+							return
+						}
+						_, err = io.Copy(os.Stdout, imgLoadResp.Body)
+						if err != nil {
+							log.Printf("Error copying to stdout: %v\n", err)
+							return
+						}
+
+						// p = path.Join("miner", "job", m.Job.ID.String(), "data")
+						// resp, err = get(p, authToken, jobToken)
+						// if err != nil {
+						// 	log.Printf("Error GET %v: %v\n", p, err)
+						// 	return
+						// }
+						// defer check.Err(resp.Body.Close)
 
 						// TODO: use job authToken to .. run?
-					}()
+						// TODO: do I need to preserve users' file structure?
+						// [relative pathing between train.py and path/to/data/]
+						// wd, err := os.Getwd()
+						// if err != nil {
+						// 	log.Printf("Error getting working directory: %v\n", err)
+						// 	http.Error(w, err.Error(), http.StatusInternalServerError)
+						// 	return
+						// }
+						// hostDataPath := filepath.Join(wd, userDir, "data")
+						// dockerDataPath := filepath.Join(userHome, "data")
+						c, err := cli.ContainerCreate(ctx, &container.Config{
+							Image: m.Job.ID.String(),
+							Tty:   true,
+						}, &container.HostConfig{
+							AutoRemove: true,
+							// Binds: []string{
+							// 	fmt.Sprintf("%s:%s:ro", hostDataPath, dockerDataPath),
+							// },
+							CapDrop: []string{
+								"ALL",
+							},
+							// TODO: mount a rw drive and use readonlyrootfs
+							// ReadonlyRootfs: true,
+							Runtime: "nvidia",
+							SecurityOpt: []string{
+								"no-new-privileges",
+							},
+						}, nil, "")
+						if err != nil {
+							log.Printf("Error creating container: %v\n", err)
+							return
+						}
 
-					// 	zr, err := zlib.NewReader(r)
-					// 	if err != nil {
-					// 		log.Printf("Error decompressing image: %v\n", err)
-					// 		break
-					// 	}
-					//
-					// 	ctx := context.Background()
-					// 	cli, err := docker.NewEnvClient()
-					// 	imgResp, err := cli.ImageLoad(ctx, zr, false)
-					// 	if err != nil {
-					// 		log.Printf("Error loading image: %v\n", err)
-					// 		break
-					// 	}
-					// 	err = zr.Close()
-					// 	if err != nil {
-					// 		log.Printf("Error closing zlib reader: %v\n", err)
-					// 		break
-					// 	}
-					// 	log.Printf("Image successfully loaded!\n")
-					// 	_, err = io.Copy(os.Stdout, imgResp.Body)
-					//
-					// 	// TODO: do I need to preserve users' file structure?
-					// 	// [relative pathing between train.py and path/to/data/]
-					// 	// wd, err := os.Getwd()
-					// 	// if err != nil {
-					// 	// 	log.Printf("Error getting working directory: %v\n", err)
-					// 	// 	http.Error(w, err.Error(), http.StatusInternalServerError)
-					// 	// 	return
-					// 	// }
-					// 	// hostDataPath := filepath.Join(wd, userDir, "data")
-					// 	// dockerDataPath := filepath.Join(userHome, "data")
-					// 	// resp, err := cli.ContainerCreate(ctx, &container.Config{
-					// 	// 	// Image: j.ID.String(),
-					// 	// 	Image: string(id),
-					// 	// 	Tty:   true,
-					// 	// }, &container.HostConfig{
-					// 	// 	AutoRemove: true,
-					// 	// 	// Binds: []string{
-					// 	// 	// 	fmt.Sprintf("%s:%s:ro", hostDataPath, dockerDataPath),
-					// 	// 	// },
-					// 	// 	CapDrop: []string{
-					// 	// 		"ALL",
-					// 	// 	},
-					// 	// 	// TODO: mount a rw drive and use readonlyrootfs
-					// 	// 	// ReadonlyRootfs: true,
-					// 	// 	Runtime: "nvidia",
-					// 	// 	SecurityOpt: []string{
-					// 	// 		"no-new-privileges",
-					// 	// 	},
-					// 	// }, nil, "")
-					// 	// if err != nil {
-					// 	// 	log.Printf("Error creating container: %v\n", err)
-					// 	// 	return
-					// 	// }
-					// 	//
-					// 	// if err := cli.ContainerStart(ctx, resp.ID, types.ContainerStartOptions{}); err != nil {
-					// 	// 	log.Printf("Error starting container: %v\n", err)
-					// 	// 	return
-					// 	// }
-					// 	//
-					// 	// out, err := cli.ContainerLogs(ctx, resp.ID, types.ContainerLogsOptions{
-					// 	// 	Follow:     true,
-					// 	// 	ShowStdout: true,
-					// 	// })
-					// 	// if err != nil {
-					// 	// 	log.Printf("Error logging container: %v\n", err)
-					// 	// 	return
-					// 	// }
-					// 	//
-					// 	// // tee := io.TeeReader(out, fw)
-					// 	// // _, err = io.Copy(os.Stdout, tee)
-					// 	// // if err != nil && err != io.EOF {
-					// 	// // 	log.Printf("Error copying to stdout: %v\n", err)
-					// 	// // 	return
-					// 	// // }
-					// 	// scanner := bufio.NewScanner(out)
-					// 	// for scanner.Scan() {
-					// 	// 	log.Println(scanner.Text())
-					// 	// }
-					// 	// // response <- []byte(out)
-					// 	//
-					// 	// err = out.Close()
-					// 	// if err != nil {
-					// 	// 	log.Printf("Error closing container log: %v\n", err)
-					// 	// 	break
-					// 	// }
-					//
-					// 	err = imgResp.Body.Close()
-					// 	if err != nil {
-					// 		log.Printf("Error closing zlib reader: %v\n", err)
-					// 		break
-					// 	}
-					// case "Data\n":
-					// 	log.Printf("Incoming data\n")
-					// default:
-					// 	_, err = io.Copy(os.Stdout, &b)
-					// 	if err != nil && err != io.EOF {
-					// 		log.Printf("Error copying websocket.TextMessage to os.Stdout: %v\n", err)
-					// 		break
-					// 	}
-					// }
+						if err := cli.ContainerStart(ctx, c.ID, types.ContainerStartOptions{}); err != nil {
+							log.Printf("Error starting container: %v\n", err)
+							return
+						}
+
+						out, err := cli.ContainerLogs(ctx, c.ID, types.ContainerLogsOptions{
+							Follow:     true,
+							ShowStdout: true,
+						})
+						if err != nil {
+							log.Printf("Error logging container: %v\n", err)
+							return
+						}
+						//
+						// // tee := io.TeeReader(out, fw)
+						// // _, err = io.Copy(os.Stdout, tee)
+						// // if err != nil && err != io.EOF {
+						// // 	log.Printf("Error copying to stdout: %v\n", err)
+						// // 	return
+						// // }
+						scanner := bufio.NewScanner(out)
+						for scanner.Scan() {
+							log.Println(scanner.Text())
+						}
+						// // response <- []byte(out)
+						//
+						// err = out.Close()
+						// if err != nil {
+						// 	log.Printf("Error closing container log: %v\n", err)
+						// 	break
+						// }
+					}()
 
 				case websocket.TextMessage:
 					resp := "Error -- unexpected text message received.\n"
 					log.Printf(resp)
 					_, err = io.Copy(os.Stdout, r)
 					response <- []byte(resp)
-
 				default:
 					log.Printf("Non-text or -binary websocket message received. Closing.\n")
 					return
