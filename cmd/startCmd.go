8d4184c0aa67fa2eb51d7e897f38e79b5fd473fd
wminshew <wminshew@gmail.com>
2018-05-26 01:32:11 -0700

    minor issues with dep; moved bulk of bidding & follow through execution code to own named function

--- cmd/startCmd.go
+++ cmd/startCmd.go
@@ -73,222 +73,7 @@ will default to the mining command provided in
 					}
 					log.Printf("Job: %+v\n", m.Job)
 
-					go func() {
-						client := resolveClient()
-						b := &job.Bid{
-							MinRate: 0.2,
-						}
-						log.Printf("Sending bid: %+v\n", b)
-
-						var body bytes.Buffer
-						p := path.Join("miner", "job", m.Job.ID.String(), "bid")
-						err = json.NewEncoder(&body).Encode(b)
-						if err != nil {
-							log.Printf("Error encoding json bid: %v\n", err)
-							return
-						}
-						req, err := postReq(p, authToken, &body)
-						if err != nil {
-							log.Printf("Error creating request POST %v: %v\n", p, err)
-							return
-						}
-						log.Printf("%v %v\n", req.Method, p)
-						resp, err := client.Do(req)
-						if err != nil {
-							log.Printf("Error %v %v: %v\n", req.Method, p, err)
-							return
-						}
-
-						if appEnv == "dev" {
-							respDump, err := httputil.DumpResponse(resp, true)
-							if err != nil {
-								log.Println(err)
-							}
-							log.Println(string(respDump))
-						}
-
-						if resp.StatusCode != http.StatusOK {
-							log.Printf("Response header error: %v\n", resp.Status)
-							check.Err(resp.Body.Close)
-							return
-						}
-
-						jobToken := resp.Header.Get("Set-Job-Authorization")
-						if jobToken == "" {
-							log.Printf("Sorry, your bid (%+v) did not win.\n", b)
-							check.Err(resp.Body.Close)
-							return
-						}
-
-						_, _ = io.Copy(ioutil.Discard, resp.Body)
-						check.Err(resp.Body.Close)
-
-						p = path.Join("miner", "job", m.Job.ID.String(), "image")
-						req, err = getJobReq(p, authToken, jobToken)
-						if err != nil {
-							log.Printf("Error creating request GET %v: %v\n", p, err)
-							return
-						}
-						log.Printf("%v %v\n", req.Method, p)
-						resp, err = client.Do(req)
-						if err != nil {
-							log.Printf("Error %v %v: %v\n", req.Method, p, err)
-							return
-						}
-
-						if resp.StatusCode != http.StatusOK {
-							log.Printf("Response header error: %v\n", resp.Status)
-							check.Err(resp.Body.Close)
-							return
-						}
-
-						zResp, err := zlib.NewReader(resp.Body)
-						if err != nil {
-							log.Printf("Error creating zlib img reader: %v\n", err)
-							check.Err(resp.Body.Close)
-							return
-						}
-
-						ctx := context.Background()
-						cli, err := docker.NewEnvClient()
-						if err != nil {
-							log.Printf("Error creating docker client: %v\n", err)
-							check.Err(resp.Body.Close)
-							check.Err(zResp.Close)
-							return
-						}
-						imgLoadResp, err := cli.ImageLoad(ctx, zResp, false)
-						if err != nil {
-							log.Printf("Error loading image: %v\n", err)
-							check.Err(resp.Body.Close)
-							check.Err(zResp.Close)
-							return
-						}
-						defer check.Err(imgLoadResp.Body.Close)
-						defer func() {
-							imgDelResp, err := cli.ImageRemove(ctx, m.Job.ID.String(), types.ImageRemoveOptions{
-								Force: true,
-							})
-							if err != nil {
-								log.Printf("Error deleting image %v: %v\n", m.Job.ID.String(), err)
-								return
-							}
-							for i := range imgDelResp {
-								log.Printf("Deleted: %v", imgDelResp[i].Deleted)
-								log.Printf("Untagged: %v", imgDelResp[i].Untagged)
-							}
-						}()
-						err = zResp.Close()
-						if err != nil {
-							log.Printf("Error closing zlib img reader: %v\n", err)
-							check.Err(resp.Body.Close)
-							return
-						}
-
-						_, _ = io.Copy(ioutil.Discard, resp.Body)
-						check.Err(resp.Body.Close)
-
-						p = path.Join("miner", "job", m.Job.ID.String(), "data")
-						req, err = getJobReq(p, authToken, jobToken)
-						if err != nil {
-							log.Printf("Error creating request GET %v: %v\n", p, err)
-							return
-						}
-						log.Printf("%v %v\n", req.Method, p)
-						resp, err = client.Do(req)
-						if err != nil {
-							log.Printf("Error %v %v: %v\n", req.Method, p, err)
-							return
-						}
-
-						if resp.StatusCode != http.StatusOK {
-							log.Printf("Response header error: %v\n", resp.Status)
-							check.Err(resp.Body.Close)
-							return
-						}
-
-						wd, err := os.Getwd()
-						if err != nil {
-							log.Printf("Error getting working directory: %v\n", err)
-							check.Err(resp.Body.Close)
-							return
-						}
-						hostDataDir := path.Join(wd, ".emrysminer", "temp-job-data")
-						hostDataPath := path.Join(hostDataDir, "data")
-
-						if err = os.MkdirAll(hostDataPath, 0755); err != nil {
-							log.Printf("Error making data dir %v: %v\n", hostDataPath, err)
-							check.Err(resp.Body.Close)
-							return
-						}
-						if err = archiver.TarGz.Read(resp.Body, hostDataDir); err != nil {
-							log.Printf("Error unpacking .tar.gz into data dir %v: %v\n", hostDataPath, err)
-							check.Err(resp.Body.Close)
-							return
-						}
-						check.Err(resp.Body.Close)
-						defer check.Err(func() error { return os.RemoveAll(hostDataDir) })
-						userHome := "/home/user"
-						dockerDataPath := path.Join(userHome, "data")
-						c, err := cli.ContainerCreate(ctx, &container.Config{
-							Image: m.Job.ID.String(),
-							Tty:   true,
-						}, &container.HostConfig{
-							AutoRemove: true,
-							Binds: []string{
-								fmt.Sprintf("%s:%s:ro", hostDataPath, dockerDataPath),
-							},
-							CapDrop: []string{
-								"ALL",
-							},
-							// TODO: mount a rw drive and use readonlyrootfs
-							// ReadonlyRootfs: true,
-							Runtime: "nvidia",
-							SecurityOpt: []string{
-								"no-new-privileges",
-							},
-						}, nil, "")
-						if err != nil {
-							log.Printf("Error creating container: %v\n", err)
-							return
-						}
-
-						if err := cli.ContainerStart(ctx, c.ID, types.ContainerStartOptions{}); err != nil {
-							log.Printf("Error starting container: %v\n", err)
-							return
-						}
-
-						out, err := cli.ContainerLogs(ctx, c.ID, types.ContainerLogsOptions{
-							Follow:     true,
-							ShowStdout: true,
-						})
-						if err != nil {
-							log.Printf("Error logging container: %v\n", err)
-							return
-						}
-
-						p = path.Join("miner", "job", m.Job.ID.String(), "output", "log")
-						req, err = postJobReq(p, authToken, jobToken, out)
-						if err != nil {
-							log.Printf("Error creating request POST %v: %v\n", p, err)
-							return
-						}
-						log.Printf("%v %v\n", req.Method, p)
-						resp, err = client.Do(req)
-						if err != nil {
-							log.Printf("Error %v %v: %v\n", req.Method, p, err)
-							return
-						}
-
-						if resp.StatusCode != http.StatusOK {
-							log.Printf("Response header error: %v\n", resp.Status)
-							check.Err(resp.Body.Close)
-							return
-						}
-
-						check.Err(resp.Body.Close)
-					}()
-
+					go bid(authToken, m)
 				case websocket.TextMessage:
 					resp := "Error -- unexpected text message received.\n"
 					log.Printf(resp)
@@ -400,4 +185,220 @@ func postJobReq(path, authToken, jobToken string, body io.Reader) (*http.Request
 	req.Header.Set("Job-Authorization", jobToken)
 
 	return req, nil
+}
+
+func bid(authToken string, m *job.Message) {
+	client := resolveClient()
+	b := &job.Bid{
+		MinRate: 0.2,
+	}
+	log.Printf("Sending bid: %+v\n", b)
+
+	var body bytes.Buffer
+	p := path.Join("miner", "job", m.Job.ID.String(), "bid")
+	err := json.NewEncoder(&body).Encode(b)
+	if err != nil {
+		log.Printf("Error encoding json bid: %v\n", err)
+		return
+	}
+	req, err := postReq(p, authToken, &body)
+	if err != nil {
+		log.Printf("Error creating request POST %v: %v\n", p, err)
+		return
+	}
+	log.Printf("%v %v\n", req.Method, p)
+	resp, err := client.Do(req)
+	if err != nil {
+		log.Printf("Error %v %v: %v\n", req.Method, p, err)
+		return
+	}
+
+	if appEnv == "dev" {
+		respDump, err := httputil.DumpResponse(resp, true)
+		if err != nil {
+			log.Println(err)
+		}
+		log.Println(string(respDump))
+	}
+
+	if resp.StatusCode != http.StatusOK {
+		log.Printf("Response header error: %v\n", resp.Status)
+		check.Err(resp.Body.Close)
+		return
+	}
+
+	jobToken := resp.Header.Get("Set-Job-Authorization")
+	if jobToken == "" {
+		log.Printf("Sorry, your bid (%+v) did not win.\n", b)
+		check.Err(resp.Body.Close)
+		return
+	}
+
+	_, _ = io.Copy(ioutil.Discard, resp.Body)
+	check.Err(resp.Body.Close)
+
+	p = path.Join("miner", "job", m.Job.ID.String(), "image")
+	req, err = getJobReq(p, authToken, jobToken)
+	if err != nil {
+		log.Printf("Error creating request GET %v: %v\n", p, err)
+		return
+	}
+	log.Printf("%v %v\n", req.Method, p)
+	resp, err = client.Do(req)
+	if err != nil {
+		log.Printf("Error %v %v: %v\n", req.Method, p, err)
+		return
+	}
+
+	if resp.StatusCode != http.StatusOK {
+		log.Printf("Response header error: %v\n", resp.Status)
+		check.Err(resp.Body.Close)
+		return
+	}
+
+	zResp, err := zlib.NewReader(resp.Body)
+	if err != nil {
+		log.Printf("Error creating zlib img reader: %v\n", err)
+		check.Err(resp.Body.Close)
+		return
+	}
+
+	ctx := context.Background()
+	cli, err := docker.NewEnvClient()
+	if err != nil {
+		log.Printf("Error creating docker client: %v\n", err)
+		check.Err(resp.Body.Close)
+		check.Err(zResp.Close)
+		return
+	}
+	imgLoadResp, err := cli.ImageLoad(ctx, zResp, false)
+	if err != nil {
+		log.Printf("Error loading image: %v\n", err)
+		check.Err(resp.Body.Close)
+		check.Err(zResp.Close)
+		return
+	}
+	defer check.Err(imgLoadResp.Body.Close)
+	defer func() {
+		imgDelResp, err := cli.ImageRemove(ctx, m.Job.ID.String(), types.ImageRemoveOptions{
+			Force: true,
+		})
+		if err != nil {
+			log.Printf("Error deleting image %v: %v\n", m.Job.ID.String(), err)
+			return
+		}
+		for i := range imgDelResp {
+			log.Printf("Deleted: %v", imgDelResp[i].Deleted)
+			log.Printf("Untagged: %v", imgDelResp[i].Untagged)
+		}
+	}()
+	err = zResp.Close()
+	if err != nil {
+		log.Printf("Error closing zlib img reader: %v\n", err)
+		check.Err(resp.Body.Close)
+		return
+	}
+
+	_, _ = io.Copy(ioutil.Discard, resp.Body)
+	check.Err(resp.Body.Close)
+
+	p = path.Join("miner", "job", m.Job.ID.String(), "data")
+	req, err = getJobReq(p, authToken, jobToken)
+	if err != nil {
+		log.Printf("Error creating request GET %v: %v\n", p, err)
+		return
+	}
+	log.Printf("%v %v\n", req.Method, p)
+	resp, err = client.Do(req)
+	if err != nil {
+		log.Printf("Error %v %v: %v\n", req.Method, p, err)
+		return
+	}
+
+	if resp.StatusCode != http.StatusOK {
+		log.Printf("Response header error: %v\n", resp.Status)
+		check.Err(resp.Body.Close)
+		return
+	}
+
+	wd, err := os.Getwd()
+	if err != nil {
+		log.Printf("Error getting working directory: %v\n", err)
+		check.Err(resp.Body.Close)
+		return
+	}
+	hostDataDir := path.Join(wd, ".emrysminer", "temp-job-data")
+	hostDataPath := path.Join(hostDataDir, "data")
+
+	if err = os.MkdirAll(hostDataPath, 0755); err != nil {
+		log.Printf("Error making data dir %v: %v\n", hostDataPath, err)
+		check.Err(resp.Body.Close)
+		return
+	}
+	if err = archiver.TarGz.Read(resp.Body, hostDataDir); err != nil {
+		log.Printf("Error unpacking .tar.gz into data dir %v: %v\n", hostDataPath, err)
+		check.Err(resp.Body.Close)
+		return
+	}
+	check.Err(resp.Body.Close)
+	defer check.Err(func() error { return os.RemoveAll(hostDataDir) })
+	userHome := "/home/user"
+	dockerDataPath := path.Join(userHome, "data")
+	c, err := cli.ContainerCreate(ctx, &container.Config{
+		Image: m.Job.ID.String(),
+		Tty:   true,
+	}, &container.HostConfig{
+		AutoRemove: true,
+		Binds: []string{
+			fmt.Sprintf("%s:%s:ro", hostDataPath, dockerDataPath),
+		},
+		CapDrop: []string{
+			"ALL",
+		},
+		// TODO: mount a rw drive and use readonlyrootfs
+		// ReadonlyRootfs: true,
+		Runtime: "nvidia",
+		SecurityOpt: []string{
+			"no-new-privileges",
+		},
+	}, nil, "")
+	if err != nil {
+		log.Printf("Error creating container: %v\n", err)
+		return
+	}
+
+	if err := cli.ContainerStart(ctx, c.ID, types.ContainerStartOptions{}); err != nil {
+		log.Printf("Error starting container: %v\n", err)
+		return
+	}
+
+	out, err := cli.ContainerLogs(ctx, c.ID, types.ContainerLogsOptions{
+		Follow:     true,
+		ShowStdout: true,
+	})
+	if err != nil {
+		log.Printf("Error logging container: %v\n", err)
+		return
+	}
+
+	p = path.Join("miner", "job", m.Job.ID.String(), "output", "log")
+	req, err = postJobReq(p, authToken, jobToken, out)
+	if err != nil {
+		log.Printf("Error creating request POST %v: %v\n", p, err)
+		return
+	}
+	log.Printf("%v %v\n", req.Method, p)
+	resp, err = client.Do(req)
+	if err != nil {
+		log.Printf("Error %v %v: %v\n", req.Method, p, err)
+		return
+	}
+
+	if resp.StatusCode != http.StatusOK {
+		log.Printf("Response header error: %v\n", resp.Status)
+		check.Err(resp.Body.Close)
+		return
+	}
+
+	check.Err(resp.Body.Close)
 }
