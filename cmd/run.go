4b31ee7f84fa735e3bf25680d1570cf0864ee83a
wminshew <wminshew@gmail.com>
2018-05-26 01:11:46 -0700

    updated package location of check

--- cmd/run.go
+++ cmd/run.go
@@ -7,7 +7,7 @@ import (
 	"github.com/mholt/archiver"
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
-	"github.com/wminshew/check"
+	"github.com/wminshew/emrys/pkg/check"
 	"io"
 	"io/ioutil"
 	"log"
