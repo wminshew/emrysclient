cf61bef0eed739c318f2d858fd6e0f50a409e597
wminshew <wminshew@gmail.com>
2018-05-29 17:37:11 -0700

    settled versioning question

--- cmd/version.go
+++ cmd/version.go
@@ -10,7 +10,6 @@ var versionCmd = &cobra.Command{
 	Short: "Print the version number of emrysminer",
 	Long:  `All software has versions. This is emrysminer's`,
 	Run: func(cmd *cobra.Command, args []string) {
-		// TODO: decide on a versioning scheme; likely semver
-		fmt.Println("emrysminer v0.1")
+		fmt.Println("emrysminer v0.1.0")
 	},
 }
