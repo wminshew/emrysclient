1cc50cd24119e08bb67c4238b357d28e96aeb64a
wminshew <wminshew@gmail.com>
2018-08-22 13:05:00 -0700

    fixed error when no data dir to download

--- cmd/downloadData.go
+++ cmd/downloadData.go
@@ -6,6 +6,7 @@ import (
 	"github.com/cenkalti/backoff"
 	"github.com/mholt/archiver"
 	"github.com/wminshew/emrys/pkg/check"
+	// "io"
 	"io/ioutil"
 	"log"
 	"net/http"
@@ -22,42 +23,42 @@ func downloadData(ctx context.Context, wg *sync.WaitGroup, errCh chan<- error, c
 	u.Path = p
 	req, err := http.NewRequest(m, u.String(), nil)
 	if err != nil {
-		log.Printf("Failed to create http request %v %v: %v\n", m, p, err)
+		log.Printf("Data: failed to create http request %v %v: %v\n", m, p, err)
 		errCh <- err
 		return
 	}
 	req.Header.Set("Authorization", fmt.Sprintf("Bearer %v", authToken))
 
-	log.Printf("Downloading data...\n")
+	log.Printf("Data: downloading...\n")
 	var resp *http.Response
 	operation := func() error {
 		var err error
 		if resp, err = client.Do(req); err != nil {
 			return err
 		}
+		defer check.Err(resp.Body.Close)
 
 		if resp.StatusCode != http.StatusOK {
-			log.Printf("http request error %v %v\n", m, p)
-			log.Printf("Response error header: %v\n", resp.Status)
+			log.Printf("Data: error %v %v\n", m, p)
+			log.Printf("Data: response header: %v\n", resp.Status)
 			b, _ := ioutil.ReadAll(resp.Body)
-			log.Printf("Response error detail: %s", b)
-			check.Err(resp.Body.Close)
+			log.Printf("Data: response detail: %s", b)
 			return fmt.Errorf("%v", b)
+		}
+
+		if resp.ContentLength != 0 {
+			if err = archiver.TarGz.Read(resp.Body, jobDir); err != nil {
+				log.Printf("Data: error unpacking .tar.gz into job dir %v: %v\n", jobDir, err)
+				return err
+			}
 		}
 
 		return nil
 	}
 	if err := backoff.Retry(operation, backoff.NewExponentialBackOff()); err != nil {
-		log.Printf("Error %v %v: %v\n", req.Method, req.URL.Path, err)
+		log.Printf("Data: error %v %v: %v\n", req.Method, req.URL.Path, err)
 		errCh <- err
 		return
 	}
-	defer check.Err(resp.Body.Close)
-
-	if err = archiver.TarGz.Read(resp.Body, jobDir); err != nil {
-		log.Printf("Error unpacking .tar.gz into job dir %v: %v\n", jobDir, err)
-		errCh <- err
-		return
-	}
-	log.Printf("Data downloaded!\n")
+	log.Printf("Data: downloaded!\n")
 }
