4b7a66facebebe4738df22bb55de9c7660ed9f1c
wminshew <wminshew@gmail.com>
2018-09-04 16:30:33 -0700

    removed exp backoff from auction; don't retry if fails

--- cmd/runAuction.go
+++ cmd/runAuction.go
@@ -3,14 +3,12 @@ package cmd
 import (
 	"context"
 	"fmt"
-	"github.com/cenkalti/backoff"
 	"github.com/wminshew/emrys/pkg/check"
 	"io/ioutil"
 	"log"
 	"net/http"
 	"net/url"
 	"path"
-	"time"
 )
 
 func runAuction(ctx context.Context, client *http.Client, u url.URL, jID, authToken string) error {
@@ -25,27 +23,15 @@ func runAuction(ctx context.Context, client *http.Client, u url.URL, jID, authTo
 	req = req.WithContext(ctx)
 	req.Header.Set("Authorization", fmt.Sprintf("Bearer %v", authToken))
 
-	operation := func() error {
-		resp, err := client.Do(req)
-		if err != nil {
-			return err
-		}
-		defer check.Err(resp.Body.Close)
-
-		if resp.StatusCode != http.StatusOK {
-			b, _ := ioutil.ReadAll(resp.Body)
-			return fmt.Errorf("server response: %s", b)
-		}
-
-		return nil
+	resp, err := client.Do(req)
+	if err != nil {
+		return err
 	}
-	if err := backoff.RetryNotify(operation,
-		backoff.WithContext(backoff.WithMaxRetries(backoff.NewExponentialBackOff(), 5), ctx),
-		func(err error, t time.Duration) {
-			log.Printf("Auction error: %v", err)
-			log.Printf("Trying again in %s seconds\n", t.Round(time.Second).String())
-		}); err != nil {
-		return fmt.Errorf("%v", err)
+	defer check.Err(resp.Body.Close)
+
+	if resp.StatusCode != http.StatusOK {
+		b, _ := ioutil.ReadAll(resp.Body)
+		return fmt.Errorf("server response: %s", b)
 	}
 
 	log.Printf("Miner selected!\n")
