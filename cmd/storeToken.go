f8d6086cdd4f3189d510452144a4a858b37e0b2e
wminshew <wminshew@gmail.com>
2018-10-04 17:04:38 -0700

    tightened file permissions on stored token (user rw only)

--- cmd/storeToken.go
+++ cmd/storeToken.go
@@ -10,8 +10,6 @@ import (
 )
 
 func storeToken(t string) error {
-	var perm os.FileMode
-	perm = 0755
 	u, err := user.Current()
 	if err != nil {
 		log.Printf("Failed to get current user: %v", err)
@@ -19,11 +17,11 @@ func storeToken(t string) error {
 	}
 	configDir := path.Join(u.HomeDir, ".config", "emrys")
 	p := path.Join(configDir, "jwt")
-	if err := os.MkdirAll(filepath.Dir(p), perm); err != nil {
+	if err := os.MkdirAll(filepath.Dir(p), 0700); err != nil {
 		log.Printf("Failed to make directory %s to save login token: %v", configDir, err)
 		return err
 	}
-	if err := ioutil.WriteFile(p, []byte(t), perm); err != nil {
+	if err := ioutil.WriteFile(p, []byte(t), 0600); err != nil {
 		log.Printf("Failed to write login token to disk at %s: %v", p, err)
 		return err
 	}
