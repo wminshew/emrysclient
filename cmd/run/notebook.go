4b6e0d4fbc9698ead210b888b57a0e47bf9a326d
wminshew <wminshew@gmail.com>
2019-02-07 13:33:25 -0800

    moved sshLocalForward to own file

--- cmd/run/notebook.go
+++ cmd/run/notebook.go
@@ -14,7 +14,6 @@ import (
 	"net/http"
 	"net/url"
 	"os"
-	"os/exec"
 	"os/signal"
 	"os/user"
 	"path/filepath"
@@ -236,15 +235,4 @@ var NotebookCmd = &cobra.Command{
 
 		log.Printf("Complete!\n")
 	},
-}
-
-func (j *userJob) sshLocalForward(ctx context.Context, sshKeyFile string) error {
-	// ssh -v -i {id_rsa} -N -L 8888:/home/{jID}/notebook.sock -p 2222 {jID}@notebook.emrys.io -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null
-	cmdStr := "ssh"
-	// TODO: change port? what if running notebooks for two different people?
-	// TODO: consider using unix domain sockets inside /home/emrys/.config/{jID} ?
-	// TODO: make local port settable by user
-	args := []string{"-v", "-i", sshKeyFile, "-N", "-L", fmt.Sprintf("8888:/home/%s/notebook.sock", j.id), "-p", "2222", fmt.Sprintf("%s@notebook.emrys.io", j.id), "-o", "StrictHostKeyChecking=no", "-o", "UserKnownHostsFile=/dev/null"}
-	cmd := exec.CommandContext(ctx, cmdStr, args...)
-	return cmd.Run()
 }
---
+++ cmd/run/sshLocalForward.go
@@ -0,0 +1,18 @@
+package run
+
+import (
+	"context"
+	"fmt"
+	"os/exec"
+)
+
+func (j *userJob) sshLocalForward(ctx context.Context, sshKeyFile string) error {
+	// ssh -v -i {id_rsa} -N -L 8888:/home/{jID}/notebook.sock -p 2222 {jID}@notebook.emrys.io -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null
+	cmdStr := "ssh"
+	// TODO: change port? what if running notebooks for two different people?
+	// TODO: consider using unix domain sockets inside /home/emrys/.config/{jID} ?
+	// TODO: make local port settable by user
+	args := []string{"-v", "-i", sshKeyFile, "-N", "-L", fmt.Sprintf("8888:/home/%s/notebook.sock", j.id), "-p", "2222", fmt.Sprintf("%s@notebook.emrys.io", j.id), "-o", "StrictHostKeyChecking=no", "-o", "UserKnownHostsFile=/dev/null"}
+	cmd := exec.CommandContext(ctx, cmdStr, args...)
+	return cmd.Run()
+}
