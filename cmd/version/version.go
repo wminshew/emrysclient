1279f72d4a95b7fc928607c6d12c1f8870a962ee
wminshew <wminshew@gmail.com>
2019-04-23 00:21:28 -0700

    updated semvers

--- cmd/version/version.go
+++ cmd/version/version.go
@@ -21,14 +21,14 @@ import (
 var UserVer = semver.Version{
 	Major: 0,
 	Minor: 6,
-	Patch: 1,
+	Patch: 3,
 }
 
 // MinerVer is the semver miner client version
 var MinerVer = semver.Version{
 	Major: 0,
 	Minor: 6,
-	Patch: 1,
+	Patch: 3,
 }
 
 const (
