13882cc23b25a8a4e10c1c525889fcf496313ed2
wminshew <wminshew@gmail.com>
2019-10-17 22:39:42 -0700

    retry connection on internal errors

--- cmd/mine/mine.go
+++ cmd/mine/mine.go
@@ -454,6 +454,8 @@ var Cmd = &cobra.Command{
 
 				if resp.StatusCode == http.StatusBadGateway {
 					return fmt.Errorf("server: temporary error")
+				} else if resp.StatusCode == http.StatusInternalServerError {
+					return fmt.Errorf("server: internal error")
 				} else if resp.StatusCode >= 300 {
 					b, _ := ioutil.ReadAll(resp.Body)
 					return backoff.Permanent(fmt.Errorf("server: %v", string(b)))
